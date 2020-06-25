7ebc12fdb232eeff328f21a2c51a25ae0e3bc4ff
A.Slmn <37568451+alizasolomon@users.noreply.github.com>
2020-06-25 23:56:40 +0800

    initial

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+ScrollReveal().reveal('section .container .col', {
+  origin: 'top', 
+  viewFactor: 0.5,
+  opacity: 0,
+  viewFactor: 0.15
+});
